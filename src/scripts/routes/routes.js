a7e17a4a7a4c2bf3360dd600ee7d4fc36df9fcc9
Rauf <alfarras74@gmail.com>
2024-05-19 18:36:48 +0700

    URL Routes Website

---
+++ src/scripts/routes/routes.js
@@ -0,0 +1,12 @@
+import Home from "../views/pages/home.js";
+import Favorite from "../views/pages/favorite.js";
+import Detail from "../views/pages/detail.js";
+
+const routes = {
+    "/": Home,
+    "/home": Home,
+    "/favorite": Favorite,
+    "/detail/:id": Detail,
+};
+
+export default routes;
